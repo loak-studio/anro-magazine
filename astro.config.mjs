597a0946baf32560bbc8f24680796778a1a5857a
Benjamin Geets <benjamingeets@tutamail.com>
2022-10-03 11:23:25 +0200

    feat: social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://anro-magazine.com",
   integrations: [compress(), sitemap(), tailwind(),astroI18next()]
 });
