53ce01a0d7513f1a23fa75a99ea5ac02c9e00fc3
Benjamin Geets <benjamingeets@tutamail.com>
2022-11-04 10:50:34 +0100

    feat(articles): loop on each request pages

--- src/cms/get-articles.js
+++ src/cms/get-articles.js
@@ -1,6 +1,28 @@
 import { api } from "./config";
 
-const getArticles = async ({ lang }) =>
-  await (await fetch(api + "/api/articles?populate=*&pagination[pageSize]=99&locale=" + lang)).json();
+const getArticles = async ({ lang }) => {
+  let data = [];
+  const req = await (
+    await fetch(api + "/api/articles?populate=*&locale=" + lang)
+  ).json();
+  data = req.data;
+  console.log("Getting articles page =>", req.meta.pagination.page);
+  for (
+    let currentPage = req.meta.pagination.page + 1;
+    currentPage <= req.meta.pagination.pageCount;
+    currentPage++
+  ) {
+    const currentReq = await (
+      await fetch(
+        api +
+          `/api/articles?populate=*&pagination[page]=${currentPage}&locale=` +
+          lang
+      )
+    ).json();
+    console.log("Getting articles page =>", currentReq.meta.pagination.page);
+    data = [...data, ...currentReq.data];
+  }
+  return { data };
+};
 
 export { getArticles };
