726a3e36b5b3d546af1b7829a56368bc617b6994
Benjamin Geets <benjamingeets@tutamail.com>
2022-11-04 10:27:27 +0100

    fix(articles): up limit to 99

--- src/cms/get-articles.js
+++ src/cms/get-articles.js
@@ -1,6 +1,6 @@
 import { api } from "./config";
 
 const getArticles = async ({ lang }) =>
-  await (await fetch(api + "/api/articles?populate=*&locale=" + lang)).json();
+  await (await fetch(api + "/api/articles?populate=*&pagination[pageSize]=99&locale=" + lang)).json();
 
 export { getArticles };
