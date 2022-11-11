c0515cef1fec1ce269d63c7428a7d1386e1b20ce
Benjamin Geets <benjamingeets@tutamail.com>
2022-11-11 15:26:40 +0100

    feat: RSS

---
+++ src/pages/en/feed.xml.js
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss";
+import i18next, { t, changeLanguage } from "i18next";
+import { getArticles } from "../../cms/get-articles";
+changeLanguage("en");
+export const get = async () => {
+  const { data } = await getArticles({ lang: i18next.language });
+  return rss({
+    title: "ANRÓ Magazine",
+    description: t("meta.description"),
+    site: import.meta.env.SITE,
+    items: data.map((post) => ({
+      link: "/en/article/" + post.attributes.slug,
+      title: post.attributes.name,
+      pubDate: post.attributes.date,
+    })),
+    customData: `<language>${i18next.language}</language>`,
+  });
+};
---
+++ src/pages/feed.xml.js
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import i18next, { t, changeLanguage } from "i18next";
+import { getArticles } from "../cms/get-articles";
+changeLanguage("fr");
+export const get = async () => {
+  const { data } = await getArticles({ lang: i18next.language });
+  return rss({
+    title: "ANRÓ Magazine",
+    description: t("meta.description"),
+    site: import.meta.env.SITE,
+    items: data.map((post) => ({
+      link: "/article/" + post.attributes.slug,
+      title: post.attributes.name,
+      pubDate: post.attributes.date,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>${i18next.language}</language>`,
+  });
+};
