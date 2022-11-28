b4fa8f9d752d15d5c5e660f7123a9b481d6db2d7
Benjamin Geets <benjamingeets@tutamail.com>
2022-11-28 11:19:01 +0100

    refactor: remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
@@ -7,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://anro-magazine.com",
-  integrations: [compress(), sitemap(), tailwind(), astroI18next()],
+  integrations: [sitemap(), tailwind(), astroI18next()],
 });
